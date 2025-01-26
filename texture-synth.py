c8d873662dce39f5ed691b36fbd84c1b834db16b
Kathy Lee <lee.j.kathy@gmail.com>
2025-01-26 14:32:56 -0800

    method 1 results and refinements

--- texture-synth.py
+++ texture-synth.py
@@ -1,35 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def randomPlacement(texture, patchSize=40, dim=400):
+def displayImage(synthesizedTexture, imageName, path):
+    plt.imshow(synthesizedTexture)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(path+imageName)
+    plt.show()
+
+def randomPlacement(texture, imageName, patchSize=40):
     # take random square blocks from input texture and 
     # place randomly onto syntehsized texture
-    print("Texture shape: ", texture.shape)
+    # print("Texture shape: ", texture.shape)
 
     textureHeight, textureWidth, _ = texture.shape
-    synthesizedTexture = np.zeros((dim, dim, texture.shape[2]))
-    print("Synthesized texture shape: ", synthesizedTexture.shape)
+    synthHeight = textureHeight*5
+    synthWidth = textureWidth*5
+    synthesizedTexture = np.zeros((synthHeight, synthWidth, texture.shape[2]))
+    # print("Synthesized texture shape: ", synthesizedTexture.shape)
 
-    for i in range(0, int(dim/patchSize)):
-        for j in range(0, int(dim/patchSize)):
+    for i in range(0, int(synthHeight/patchSize)):
+        for j in range(0, int(synthWidth/patchSize)):
             width = np.random.randint(0, textureWidth - patchSize)
             height = np.random.randint(0, textureHeight - patchSize)
             block = texture[height:height+patchSize, width:width+patchSize, :]
             row = i*patchSize
             col = j*patchSize
             synthesizedTexture[row:row+patchSize, col:col+patchSize, :] = block
-    return synthesizedTexture
 
+    displayImage(synthesizedTexture, imageName, './data/results/synthesis/method1/')
 
 #text
 imageName = 'radishes.jpg'
+imageName = 'weave.jpg'
 texture = plt.imread('./data/textures/' + imageName)
-print("texture d type: ", texture.dtype)
+
 if texture.dtype == 'uint8':
     texture = texture/255.0
 
-synthesizedTexture = randomPlacement(texture)
+if (texture.ndim != 3):
+    thirdChannel = texture
+    texture = np.stack((thirdChannel,)*3, axis=2)
 
-plt.imshow(synthesizedTexture)
-plt.axis('off')
-plt.show()
+# randomPlacement(texture, imageName)
+# overlapNeighbouringBlocks(texture)
+minErrorCut(texture)
